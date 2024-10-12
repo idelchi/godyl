b075e1007ac458565c1f24b21e8ce04db6eea8fc
arash <arash@raspberry.com>
2024-10-12 17:28:51 +0200

    dev

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -1,14 +1,38 @@
+// Package env provides utilities for working with environment variables,
+// including methods to normalize, merge, retrieve, and manipulate them in a map-like structure.
 package env
 
 import (
+	"errors"
 	"fmt"
 	"os"
 	"runtime"
 	"strings"
 )
 
+var ErrEnvVarNotFound = errors.New("environment variable not found")
+
+// Env represents a map of environment variables with string keys and values.
 type Env map[string]string
 
+// FromEnv returns the current environment variables as an Env.
+func FromEnv() Env {
+	return FromSlice(os.Environ()...)
+}
+
+// FromSlice constructs an Env from a slice of key=value strings.
+func FromSlice(slice ...string) Env {
+	e := make(Env, len(slice))
+
+	for _, v := range slice {
+		kv := strings.SplitN(v, "=", 2)
+		e[kv[0]] = kv[1]
+	}
+
+	return e
+}
+
+// Normalized returns a copy of the Env with all keys normalized to uppercase on Windows.
 func (e Env) Normalized() Env {
 	if runtime.GOOS == "windows" {
 		normalized := make(Env, len(e))
@@ -23,14 +47,16 @@ func (e Env) Normalized() Env {
 	return e
 }
 
+// Get retrieves the value associated with the given key or an error if the key is not found.
 func (e Env) Get(key string) (string, error) {
 	if v, ok := e[key]; ok {
 		return v, nil
 	}
 
-	return "", fmt.Errorf("key %q not found", key)
+	return "", fmt.Errorf("%w: %q", ErrEnvVarNotFound, key)
 }
 
+// ToSlice converts the Env to a slice of `key=value“ strings.
 func (e Env) ToSlice() []string {
 	slice := make([]string, 0, len(e))
 
@@ -41,25 +67,12 @@ func (e Env) ToSlice() []string {
 	return slice
 }
 
-func FromSlice(slice ...string) Env {
-	e := make(Env, len(slice))
-
-	for _, v := range slice {
-		kv := strings.SplitN(v, "=", 2)
-		e[kv[0]] = kv[1]
-	}
-
-	return e
-}
-
+// Add adds `key=value“ pairs from a slice of strings to the Env.
 func (e *Env) Add(slice ...string) {
 	e.Merge(FromSlice(slice...))
 }
 
-func FromEnv() Env {
-	return FromSlice(os.Environ()...)
-}
-
+// Merge merges another Env into the current Env, without overwriting existing keys.
 func (e *Env) Merge(env Env) {
 	for k, v := range env {
 		if _, ok := (*e)[k]; ok {

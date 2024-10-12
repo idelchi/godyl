0f33e1e62ca677623a7fa62c6bbd8db81ff529ee
arash <arash@raspberry.com>
2024-10-12 14:46:12 +0200

    dev

--- pkg/env/env.go
+++ pkg/env/env.go
@@ -16,17 +16,19 @@ func (e Env) Get(key string) (string, error) {
 	return "", fmt.Errorf("key %q not found", key)
 }
 
-func FromEnv() Env {
-	env := os.Environ()
+func FromSlice(slice ...string) Env {
+	e := make(Env, len(slice))
 
-	e := make(Env, len(env))
-
-	for _, v := range env {
+	for _, v := range slice {
 		kv := strings.SplitN(v, "=", 2)
 		e[kv[0]] = kv[1]
 	}
 
 	return e
+}
+
+func FromEnv() Env {
+	return FromSlice(os.Environ()...)
 }
 
 func (e *Env) Merge(env Env) {

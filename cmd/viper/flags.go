178acb2f1ae3e5a2529f47dd50f5a9560e45fc1d
idelchi <64003206+idelchi@users.noreply.github.com>
2024-10-15 14:41:37 +0200

    dev

--- cmd/viper/flags.go
+++
@@ -1,7 +0,0 @@
-package main
-
-// Config holds all the configuration options for godyl.
-type Flags struct {
-	Config string
-	DotEnv string `mapstructure:"dot-env"`
-}
--- internal/match/asset.go
+++ internal/match/asset.go
@@ -32,47 +32,35 @@ func (a Asset) PlatformMatch(req Requirements) (int, bool) {
 	var score int
 	qualified := true
 
-	// fmt.Printf("Asset: %s\n", a.Name)
-
 	if a.Platform.OS != "" {
 		if a.Platform.OS == req.Platform.OS {
-			// fmt.Printf("OS %s == %s\n", a.Platform.OS, req.Platform.OS)
 			score++
 		}
 		if req.Platform.OS.IsCompatibleWith(a.Platform.OS.Name()) {
-			// fmt.Printf("OS %s compatible with %s\n", a.Platform.OS, req.Platform.OS)
 			score++
 		} else {
-			// fmt.Printf("OS %s not compatible with %s\n", a.Platform.OS, req.Platform.OS)
 			qualified = false
 		}
 	}
 
 	if a.Platform.Architecture.Name() != "" {
 		if a.Platform.Architecture.Name() == req.Platform.Architecture.Name() {
-			// fmt.Printf("Arch %s == %s\n", a.Platform.Architecture, req.Platform.Architecture)
 			score++
 		}
 		if req.Platform.Architecture.IsCompatibleWith(a.Platform.Architecture.Name(), req.Platform.Distribution) {
-			// fmt.Printf("Arch %s compatible with %s\n", a.Platform.Architecture, req.Platform.Architecture)
 			score++
 		} else {
-			// fmt.Printf("Arch %s not compatible with %s\n", a.Platform.Architecture, req.Platform.Architecture)
 			qualified = false
 		}
 	}
 
 	if a.Platform.Library != "" {
 		if a.Platform.Library == req.Platform.Library {
-			// fmt.Printf("Library %s == %s\n", a.Platform.Library, req.Platform.Library)
 			score++
 		}
 		if req.Platform.Library.IsCompatibleWith(a.Platform.Library.Name()) {
-			// fmt.Printf("Library %s compatible with %s\n", a.Platform.Library, req.Platform.Library)
 			score++
 		} else {
-			// fmt.Printf("Library %s not compatible with %s\n", a.Platform.Library, req.Platform.Library)
-			// qualified = false
 			score--
 		}
 	}

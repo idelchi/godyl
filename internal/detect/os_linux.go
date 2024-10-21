33200e29ba7b7a1f9a79f1689aa49243fccebe68
idelchi <64003206+idelchi@users.noreply.github.com>
2024-10-21 14:23:07 +0200

    dev

--- internal/detect/os_linux.go
+++ internal/detect/os_linux.go
@@ -3,6 +3,7 @@
 package detect
 
 import (
+	"fmt"
 	"runtime"
 
 	"github.com/idelchi/godyl/internal/detect/platform"
@@ -41,6 +42,7 @@ func (p *Platform) Detect() error {
 
 	if arch.Raw == "arm" {
 		arch.Version = platform.InferGoArmVersion()
+		arch.Raw = fmt.Sprintf("%sv%d", arch.Type, arch.Version)
 	}
 
 	// Populate the Platform struct with the detected values

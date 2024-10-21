4e525539a3cc41734a05f4d6462eace95bcc4181
idelchi <64003206+idelchi@users.noreply.github.com>
2024-10-21 12:29:18 +0200

    rely on GOARCH instead of `si`

--- internal/detect/os_linux.go
+++ internal/detect/os_linux.go
@@ -29,15 +29,13 @@ func (p *Platform) Detect() error {
 	}
 
 	// Determine the Linux distribution from system information
-	if err := distro.Parse(si.OS.Vendor); err != nil {
-		return err
-	}
+	distro.Parse(si.OS.Vendor)
 
 	// Set the default library based on the OS and distribution
 	library = library.Default(os, distro)
 
 	// Determine the architecture from the system's kernel architecture
-	if err := arch.Parse(si.Kernel.Architecture); err != nil {
+	if err := arch.Parse(runtime.GOARCH); err != nil {
 		return err
 	}
 

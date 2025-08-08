ed282424d198ee027f0b7a9d8ec7291ae99a7060
idelchi <64003206+idelchi@users.noreply.github.com>
2025-08-08 07:22:44 +0200

    dev

--- pkg/path/file/man.go
+++ pkg/path/file/man.go
@@ -1,7 +1,9 @@
 package file
 
 import (
+	"crypto/sha256"
 	"fmt"
+	"io/fs"
 	"net/url"
 	"os"
 	"path/filepath"
@@ -168,4 +170,59 @@ func (f File) Unescape() File {
 	}
 
 	return File(unescapedPath)
+}
+
+// Info retrieves the file information.
+func (f File) Info() (fs.FileInfo, error) {
+	info, err := os.Stat(f.String())
+	if err != nil {
+		return nil, fmt.Errorf("getting file info for %q: %w", f, err)
+	}
+
+	return info, nil
+}
+
+// Size returns the size of the file in bytes.
+func (f File) Size() (int64, error) {
+	info, err := f.Info()
+	if err != nil {
+		return 0, fmt.Errorf("getting file size for %q: %w", f, err)
+	}
+
+	if !f.IsFile() {
+		return 0, fmt.Errorf("file %q is not a regular file", f)
+	}
+
+	return info.Size(), nil
+}
+
+// LargerThan checks if the file is larger than the specified size in bytes.
+func (f File) LargerThan(size int64) (bool, error) {
+	currentSize, err := f.Size()
+	if err != nil {
+		return false, fmt.Errorf("checking if file %q is larger than %d bytes: %w", f, size, err)
+	}
+
+	return currentSize > size, nil
+}
+
+// SmallerThan checks if the file is smaller than the specified size in bytes.
+func (f File) SmallerThan(size int64) (bool, error) {
+	currentSize, err := f.Size()
+	if err != nil {
+		return false, fmt.Errorf("checking if file %q is smaller than %d bytes: %w", f, size, err)
+	}
+
+	return currentSize < size, nil
+}
+
+// Hash computes the hash of the file's contents.
+func (f File) Hash() (string, error) {
+	data, err := f.Read()
+	if err != nil {
+		return "", fmt.Errorf("reading file %q: %w", f, err)
+	}
+
+	hash := sha256.Sum256(data)
+	return fmt.Sprintf("%x", hash), nil
 }

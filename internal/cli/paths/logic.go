5eba1ac89d0134ba050843e03765e00f1501e6b7
idelchi <64003206+idelchi@users.noreply.github.com>
2025-09-13 13:16:38 +0200

    dev

--- internal/cli/paths/logic.go
+++ internal/cli/paths/logic.go
@@ -1,20 +1,28 @@
 package paths
 
 import (
-	"fmt"
-
 	"github.com/idelchi/godyl/internal/cli/core"
 	"github.com/idelchi/godyl/internal/data"
+	"github.com/idelchi/godyl/pkg/pretty"
 )
 
 // run executes the `paths` command.
 func run(input core.Input) error {
 	cfg, _, _, _, _ := input.Unpack()
 
-	fmt.Printf("config path: %s\n", cfg.ConfigFile)
-	fmt.Printf("cache path: %s\n", data.CacheFile(cfg.Cache.Dir))
-	fmt.Printf("go path: %s\n", data.GoDir())
-	fmt.Printf("temp download path: %s\n", data.DownloadDir().Path()+"-"+data.Prefix())
+	paths := struct {
+		Config string
+		Cache  string
+		Go     string
+		Temp   string
+	}{
+		Config: cfg.ConfigFile.Path(),
+		Cache:  data.CacheFile(cfg.Cache.Dir).Path(),
+		Go:     data.GoDir().Path(),
+		Temp:   data.DownloadDir().Path() + "-" + data.Prefix(),
+	}
+
+	pretty.PrintYAML(paths)
 
 	return nil
 }

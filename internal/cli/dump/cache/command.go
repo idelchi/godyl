d51bf93092cff700aa0e7a8cda206b2762394b31
idelchi <64003206+idelchi@users.noreply.github.com>
2025-09-15 19:46:56 +0200

    dev

--- internal/cli/dump/cache/command.go
+++ internal/cli/dump/cache/command.go
@@ -11,9 +11,9 @@ import (
 // Command returns the `dump cache` command.
 func Command(global *root.Config, local any) *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "cache [name]",
+		Use:   "cache [name...]",
 		Short: "Display cache information",
-		Args:  cobra.MaximumNArgs(1),
+		Args:  cobra.ArbitraryArgs,
 
 		RunE: func(cmd *cobra.Command, args []string) error {
 			// Exit early if the command is run with `--show/-s` flag.
